3c29e7d7460a172e7a8cb344c6e614b315f56947
Sch <junhg0211@gmail.com>
2023-12-02 17:48:52 +0900

    update zasokese dictionary column

--- database/zasok.py
+++ database/zasok.py
@@ -4,9 +4,10 @@ from database import Word
 class ZasokeseWord(Word):
     back_slice = 2
 
-    def __init__(self, word: str, noun: str = '', adj: str = '', verb: str = '', adv: str = '', prep: str = '',
-                 remark: str = '', derived_from_language: str = '', derived_from_word: str = ''):
+    def __init__(self, code: str = '', word: str = '', noun: str = '', adj: str = '', verb: str = '', adv: str = '',
+                 prep: str = '', remark: str = '', derived_from_language: str = '', derived_from_word: str = ''):
         super().__init__(word)
+        self.code = code
         self.noun = noun
         self.adj = adj
         self.verb = verb
@@ -15,6 +16,9 @@ class ZasokeseWord(Word):
         self.remark = remark
         self.derived_from_language = derived_from_language
         self.derived_from_word = derived_from_word
+
+    def get_field_name(self, special: bool) -> str:
+        return f'**{self.word}**#{self.code}' if not special else f'__**{self.word}**#{self.code} (일치)__'
 
     def get_field_value(self) -> str:
         definitions = list()
