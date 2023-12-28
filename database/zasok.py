89955554133f948988b11288412b0ac3cec5ce2d
Sch <junhg0211@gmail.com>
2023-12-28 23:57:07 +0900

    update zasokese column

--- database/zasok.py
+++ database/zasok.py
@@ -4,10 +4,12 @@ from database import Word
 class ZasokeseWord(Word):
     back_slice = 2
 
-    def __init__(self, code: str = '', word: str = '', noun: str = '', adj: str = '', verb: str = '', adv: str = '',
-                 prep: str = '', remark: str = '', derived_from_language: str = '', derived_from_word: str = ''):
+    def __init__(self, code: str = '', word: str = '', frequency: str = '', noun: str = '', adj: str = '', verb: str = '',
+                 adv: str = '', prep: str = '', remark: str = '', derived_from_language: str = '',
+                 derived_from_word: str = ''):
         super().__init__(word)
         self.code = code
+        self.frequency = frequency
         self.noun = noun
         self.adj = adj
         self.verb = verb
@@ -18,7 +20,8 @@ class ZasokeseWord(Word):
         self.derived_from_word = derived_from_word
 
     def get_field_name(self, special: bool) -> str:
-        return f'**{self.word}**#{self.code}' if not special else f'__**{self.word}**#{self.code} (일치)__'
+        return f'**{self.word}** {self.frequency}#{self.code}' \
+            if not special else f'__**{self.word}** {self.frequency}#{self.code} (일치)__'
 
     def get_field_value(self) -> str:
         definitions = list()
